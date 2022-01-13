c917a62b6a9aefe77df21e57c59b536e564649fb
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2022-01-13 22:13:57 +0500

    Teacher Id page add

--- pages/teachers/index.jsx
+++ pages/teachers/index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import StudentCardList from "../../components/Student/StudentCardList";
 import TechCartList from "../../components/Tech/TechCartList";
 import TechSliderList from "../../components/Tech/TechSliderList";
+import TechId from "../../components/TechId/TechId";
 
 const AboutPage = () => {
   return (
     <div>
       {/* <TechCartList /> */}
       {/* <TechSliderList /> */}
-      <StudentCardList />
+      {/* <StudentCardList /> */}
+      <TechId />
     </div>
   );
 };
