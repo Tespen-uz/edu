8296391dfef4d03cbac5c3edcb6ed386ce66ff17
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2022-02-02 13:31:30 +0500

    otimize home page

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="pt-12 lg:pt-48   max-w-2x p-2 relative   text-white py-5">
         <div className="absolute left-0 right-0 top-0"  >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#fff" fill-opacity="1" d="M0,64L80,85.3C160,107,320,149,480,160C640,171,800,149,960,133.3C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+  <path fill="#fff" fillOpacity="1" d="M0,64L80,85.3C160,107,320,149,480,160C640,171,800,149,960,133.3C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
 </svg>
         </div>
         <div className="container mx-auto flex flex-col justify-center  md:flex-row   md:justify-between  text-sm text-gray-400">
