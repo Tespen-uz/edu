ddce0c1259155888d9f16302d4a5c3d07a693253
jakhongir-abdukhamidov <jakhongirabdukhamidov@gmail.com>
2022-03-08 21:19:22 +0500

    start quiz component

---
+++ pages/demo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DemoPage = () => {
+  return (
+    <div>
+      <h1 className="text-lg text-center">Demo Page</h1>
+    </div>
+  );
+};
+
+export default DemoPage;
